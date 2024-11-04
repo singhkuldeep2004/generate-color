1237bca7dfdc704ded6cf5d7c3c3c3e70c6a2f15
Kuldeep Singh Sisodiya <kuldeepsinghsisodiya3191@gmail.com>
2024-11-04 07:44:26 +0530

    make full code

--- activity.js
+++ activity.js
@@ -1,13 +1,15 @@
 let btn = document.querySelector("button");
+let div = document.querySelector("div");
 
 btn.addEventListener("click", function(){
     console.log("generate random color");
-} )
+    getRandomColor();
+} );
 
 function getRandomColor(){
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
     let blue  = Math.floor(Math.random() * 255);
 
-    let color
+    div.style.backgroundColor = `rgb(${red},${green},${blue})`;
 }
